5dd5275b95b0540753e43e883a63c4d7f38fc81c
KarunSharma95 <mekarun121nsc@gmail.com>
2023-07-29 23:09:26 +0530

    some commented code

--- Practise1.js
+++ Practise1.js
@@ -1,3 +1,4 @@
+/*
 var a = 2;
 function square (num){
     var result =num *num;
@@ -5,4 +6,17 @@ function square (num){
 }
 var b = square (a);
 console.log(b);
+*/
+
+/*
+var x=2;
+function getName(){
+    console.log("Starting Revision of javascript")
+}
+
+getName();
+console.log(x);
+console.log(getName);
+*/
+
 
