b75c4b24d4b163cf6091d66bcc3c34120202b09d
KarunSharma95 <mekarun121nsc@gmail.com>
2023-08-05 22:37:47 +0530

    Fetch

--- script.js
+++ script.js
@@ -28,24 +28,24 @@ const countriesContainer = document.querySelector('.countries');
 // };
 // x('india');
 
-// const renderCountry= function (data1){
-//     const html =`
-//     <article class="country">
-//           <img class="country__img" src="${data1.flags.png}" />
-//           <div class="country__data">
-//             <h3 class="country__name">${data1.name.common}</h3>
-//             <h4 class="country__region">${data1.capital}</h4>
-//             <p class="country__row"><span>👫</span>${(Number(data1.population) / 1000000).toFixed(1)}</p>
-//             <p class="country__row"><span>🗣️</span>${data1.languages.hin}</p>
-//             <p class="country__row"><span>${data1.currencies.INR.symbol}</span>${data1.currencies.INR.name}</p>
+const renderCountry= function (data1){
+    const html =`
+    <article class="country">
+          <img class="country__img" src="${data1.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data1.name.common}</h3>
+            <h4 class="country__region">${data1.capital}</h4>
+            <p class="country__row"><span>👫</span>${(Number(data1.population) / 1000000).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data1.languages.hin}</p>
+            <p class="country__row"><span>${data1.currencies.INR.symbol}</span>${data1.currencies.INR.name}</p>
 
-//           </div>
-//         </article>
-//     `;
-//     countriesContainer.insertAdjacentHTML('beforeend',html);
-//     countriesContainer.style.opacity='1';
-// }
-
+          </div>
+        </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend',html);
+    countriesContainer.style.opacity='1';
+}
+/*
 let x = function (name1){
 const request = new XMLHttpRequest();
 request.open('GET',`https://restcountries.com/v3.1/name/${name1}`);
@@ -100,3 +100,12 @@ request.addEventListener('load',function(){
 });
 };
 x('india');
+*/
+
+function getCountry (country){
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response=>response.json())
+    
+    .then(data=>renderCountry(data[1]))
+};
+getCountry('india');
